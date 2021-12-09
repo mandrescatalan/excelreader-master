9003e9dcbfee80e9a22f960bc7b583bb132a508b
mandrescatalan <85004960+mandrescatalan@users.noreply.github.com>
2021-12-08 22:39:02 -0500

    Initial commit

---
+++ excelreader-master/src/app/excelsheet/clases/persona.ts
@@ -0,0 +1,4 @@
+export class Persona{
+NOMBRE: string;
+CEDULA: number;
+}
---
+++ excelreader-master/src/app/excelsheet/excelsheet.component.ts
@@ -0,0 +1,100 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { Persona } from './clases/persona';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { style } from '@angular/animations';
+
+@Component({
+  selector: 'app-excelsheet',
+  templateUrl: './excelsheet.component.html',
+  styleUrls: ['./excelsheet.component.css']
+})
+export class ExcelsheetComponent implements OnInit {
+
+
+  file: File;
+  arrayBuffer: any;
+  data: any[];
+  personas: Array<Persona>;
+  persona: Persona;
+  id: number;
+  @Input()
+  nombre: string;
+ 
+  constructor() {
+   
+  }
+
+
+  ngOnInit(): void {
+    this.persona = new Persona();
+  }
+
+  addfile(event) {
+
+    this.file = event.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(this.file);
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      var arraylist = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+      this.data = arraylist;
+      this.personas = this.getPersona(this.data)
+    }
+
+  }
+
+  getPersona(json: any[]): Array<Persona> {
+    return (json as any);
+  }
+
+  buscar(): void {
+    this.personas.forEach(element => {
+      if (element.CEDULA == this.id) {
+        this.persona = element;
+         this.nombre = element.NOMBRE;
+      }
+    });
+  }
+
+
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData');
+    const doc = new jsPDF('l', 'mm', 'a4');
+    //const options = {
+   //   background: ' ',
+     // scale: 3,
+
+    //};
+    
+
+    html2canvas(DATA).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 0;
+      const bufferY = 0;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = 378;
+      const pdfHeight = 211;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+
+  }
+
+
+}
+
